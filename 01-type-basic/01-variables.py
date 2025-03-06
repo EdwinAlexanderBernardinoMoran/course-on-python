447cf4c959a8ff449f567bf3c372dff385cd48e7
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-03-06 15:01:27 -0600

    S2-variables

---
+++ 01-type-basic/01-variables.py
@@ -0,0 +1,13 @@
+# String
+name_course = "Ultimate Python"
+
+# Integer
+num_students = 5000
+
+# Float
+average_score = 9.9
+
+# Boolean
+publicado = True
+
+print(name_course, num_students, average_score, publicado)
