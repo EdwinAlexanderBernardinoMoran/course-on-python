acf99aeb67abb6b46eb72dac17737f8643b27c46
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-03-07 09:27:32 -0600

    S2-Convertion of rates

---
+++ 01-type-basic/09-conversion-of-rates.py
@@ -0,0 +1,6 @@
+x = input("Enter the rate: ")
+
+# int() function converts the string to an integer
+# float() function converts the string to a float
+# str() function converts the number to a string
+# bool() function converts the number to a boolean
