8a88311332a43e1b4ae277fdb43d99fee1623441
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-03-07 09:20:08 -0600

    S2- Calculator

---
+++ 01-type-basic/08-calculator.py
@@ -0,0 +1,17 @@
+numberOne = input("Enter a number: ")
+numberTwo = input("Enter a number: ")
+
+numberOne = int(numberOne)
+numberTwo = int(numberTwo)
+
+sum = numberOne + numberTwo
+subtraction = numberOne - numberTwo
+multiplication = numberOne * numberTwo
+division = numberOne / numberTwo
+remainder = numberOne % numberTwo
+
+print("Sum: ", sum)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+print("Remainder: ", remainder)
