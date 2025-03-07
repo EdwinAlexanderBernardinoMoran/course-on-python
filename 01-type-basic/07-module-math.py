6431d489ba0e9aac6a584631928b0eea9dedf927
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-03-07 09:06:25 -0600

    S2-Module math

---
+++ 01-type-basic/07-module-math.py
@@ -0,0 +1,22 @@
+import math
+
+print(round(1.3))
+print(round(1.7))
+
+print(abs(-1.3))
+print(abs(1.3))
+
+# Da el valor entero más cercano al número
+print(math.ceil(1.1))
+
+# Da el valor entero más cercano al número hacia abajo
+print(math.floor(1.9999))
+
+# El valor que le pasamos correspone a que no es un número
+print(math.isnan(1.1))
+
+# Nos permite elevar un numero a la potencia de algo.
+print(math.pow(2, 3))
+
+# Nos permite sacar la raiz cuadrada de un número
+print(math.sqrt(9))
