957792ca7b4ffed9156d76490e860479b71ec303
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-06-09 15:02:50 -0600

    S3-if, else y elif

---
+++ 02-flow-control/02-if-else-elif.py
@@ -0,0 +1,11 @@
+edad = 21
+if edad > 65:
+    print("Puedes ver la pelicula con un descuento del 75%")
+elif edad > 54:
+    print("Puedes ver la pelicula con un descuento del 50%")
+elif edad > 18:
+    print("Puedes ver la pelicula")
+else:
+    print("Eres menor de edad")
+
+print("Fin del programa")
