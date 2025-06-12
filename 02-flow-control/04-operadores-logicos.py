5f3cc73dac67c778d71492cb0b74618ad8531125
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-06-11 20:51:47 -0600

    S3 - Operadores logicos

---
+++ 02-flow-control/04-operadores-logicos.py
@@ -0,0 +1,14 @@
+# Operadores lógicos en Python
+
+# AND lógico
+a = True and False  # Resultado: False
+
+# OR lógico
+b = True or False   # Resultado: True
+
+# NOT lógico
+c = not True        # Resultado: False
+
+print("AND:", a)
+print("OR:", b)
+print("NOT:", c)
