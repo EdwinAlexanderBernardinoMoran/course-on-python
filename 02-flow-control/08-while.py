4748091c91af8a2557e07d1f35bd546c0df62bd0
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-06-14 19:25:55 -0600

    S3 - Loops Anidados

--- 02-flow-control/08-while.py
+++ 02-flow-control/08-while.py
@@ -8,3 +8,16 @@ coman = ""
 while coman.lower() != "salir":
     coman = input("Introduce un comando (o 'salir' para terminar): ")
     print("Comando introducido:", coman)
+
+# Loops Infinitos
+
+while True:
+    system = input("Cual es el sistema operativo rey de los servidores ? ")
+    if system.lower() == "linux":
+        print("Correcto, Linux es el rey de los servidores")
+        break
+    else:
+        print("Incorrecto, intenta de nuevo")
+    # Puedes agregar una condición de salida si es necesario
+    # if alguna_condicion:
+    #     break
---
+++ 02-flow-control/09-loops-anidados.py
@@ -0,0 +1,3 @@
+for longitude in range(0, 10): # Outer for | Loop
+    for latitude in range(0, 10): # Inner for | Loop
+        print(f"Longitud: {longitude}, Latitud: {latitude}")
