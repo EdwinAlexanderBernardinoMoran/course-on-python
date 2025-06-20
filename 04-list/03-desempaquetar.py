f719ad4cdac009577de06e9a05f74389019326bc
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-06-20 16:09:33 -0600

    S5 - Desempaquetar listas

---
+++ 04-list/03-desempaquetar.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# firts, second, third = numbers
+firtsOther, secondOther, *others, nine, ten = numbers
+
+# print(firts, second, third)   # 1, 2, 3
+print(firtsOther, secondOther, others, nine, ten)  # 1, [2, 3]
+# Unpacking with asterisk
+
