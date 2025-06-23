d99392045aa7abf771736bac9b63a1dcd6b1940e
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-06-22 21:26:07 -0600

    S5 - Buscando elementos

---
+++ 04-list/05-buscando-elementos.py
@@ -0,0 +1,4 @@
+mascotas = ["Pelusa", "Pulga", "Felipe", "Chanchito Feliz"]
+
+mascotas.index("Felipe")  # Devuelve el índice del primer elemento encontrado
+mascotas.count("Felipe")  # Devuelve el número de veces que aparece el elemento
