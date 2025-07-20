77f863df6e7180e28aa30dda42dec4b7ffa5ecaa
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-07-20 15:21:56 -0600

    S6 - Destructor

--- 05-clases/07-metodos-magicos.py
+++ 05-clases/07-metodos-magicos.py
@@ -4,6 +4,9 @@ class Dog:
         self.name = name
         self.age = age
 
+    def __del__(self):
+        print(f"{self.name} has been deleted.")
+
     def __str__(self):
         return f"Class Dog: {self.name}, Age: {self.age}"
 
@@ -12,5 +15,6 @@ class Dog:
 
 
 dogOne = Dog("Buddy", 3)
+print(dogOne)  # This will print the object representation}
 
-print(dogOne)  # This will print the object representation
+del dogOne  # This will trigger the __del__ method
