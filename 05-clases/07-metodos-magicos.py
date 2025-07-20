289892e22e7fcc176f7828d6767e9027b0d193db
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-07-19 21:37:42 -0600

    S6 - Metodos magicos

---
+++ 05-clases/07-metodos-magicos.py
@@ -0,0 +1,16 @@
+class Dog:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Class Dog: {self.name}, Age: {self.age}"
+
+    def speak(self):
+        print(f"{self.name} says Woof!")
+
+
+dogOne = Dog("Buddy", 3)
+
+print(dogOne)  # This will print the object representation
