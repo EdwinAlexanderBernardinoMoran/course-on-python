5abcf772a3a97c67ea3d1ad43f507cb2d4211701
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-07-28 10:45:11 -0600

    S6 - Herencia

---
+++ 05-clases/10-herencia.py
@@ -0,0 +1,11 @@
+class Animal:
+    def eat(self):
+        print("The animal is eating.")
+
+class Dog(Animal):
+    def walk(self):
+        print("The dog is barking.")
+
+class Cat(Animal):
+    def programming(self):
+        print("The cat is programming.")
