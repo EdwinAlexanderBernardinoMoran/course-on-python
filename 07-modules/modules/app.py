f9fb2e2ed934197fb8daa03b08218cb77964dbe1
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-09-02 15:39:27 -0600

    S8 - dir

--- 07-modules/modules/app.py
+++ 07-modules/modules/app.py
@@ -1,6 +1,13 @@
 # from user_taxes import save, payTaxes = Importacion de modulos.
 # from users.actions.utilities import save, payTaxes # Importacion de modulos que se encuentran dentro de paquetes
 from users.taxes.utilities import payTaxes
+import users
+
+print(dir(users))
+print(users.__name__)
+print(users.__package__)
+print(users.__path__)
+print(users.__file__)
 
 # save()
 payTaxes()
